1c16d350194966ef0d72966583d56a03c814c375
kenanismayilov335 <74872919+kenanismayilov335@users.noreply.github.com>
2021-09-06 02:43:49 +0400

    Update data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 module.exports ={
-bot_token: '1912462608:AAEbsona9zgxykfw55WykF9JOBJthPyZSyU',// bot token
-admin: [1349422014], // bot admin
+bot_token: '1372871302:AAG9fdQOoLBybQzyuZNUP2AutP4jcTNB8vc',// bot token
+admin: [1135921521], // bot admin
 currency: 'LINK',// bot currency 
 bot_name:'link_cloudrobot',// bot username without @
 mongoLink:'mongodb+srv://harshit:Fqs3eyOblEUyjpSP@cluster0.p2zep.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'// mongodb cluster link
